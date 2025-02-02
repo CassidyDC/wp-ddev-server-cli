134271c2c6a7938d5e4eb5a68a96da6eab05437e
Jacob Cassidy <jacob@jacobcassidy.com>
2025-02-02 15:36:27 +0700

    Sort names alphabetically

--- utils/symbols.mjs
+++ utils/symbols.mjs
@@ -11,6 +11,6 @@ import { blue, green, orange, red, yellow } from "./colors.mjs";
 // Symbols
 export const symError = red(figures.cross);
 export const symInfo = blue(figures.info);
+export const symNote = yellow(figures.star);
 export const symSuccess = green(figures.tick);
 export const symWarning = orange(figures.warning);
-export const symNote = yellow(figures.star);
