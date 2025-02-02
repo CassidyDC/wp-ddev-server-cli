1dbd6932082a3a997eed4460db6549a90f6a3cdc
Jacob Cassidy <jacob@jacobcassidy.com>
2025-02-02 09:10:19 +0700

    Add starting `index.mjs` file

---
+++ index.mjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+/**
+ * DDEV Local WP Server CLI
+ *
+ * A simple CLI for setting up a local WordPress development server with DDEV.
+ *
+ * @author Jacob Cassidy <https://cassidydc.com>
+ */
+
+// Internal dependencies
+import { header } from "./utils/header.mjs";
+
+// Display the CLI header
+header();
+
+export default () => {
+  console.log("Working");
+};
