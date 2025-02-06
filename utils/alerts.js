449e713e934a1e06db6e880b9562fe220af87915
Jacob Cassidy <jacob@jacobcassidy.com>
2025-02-07 06:05:32 +0700

    Refactor alerts

---
+++ utils/alerts.js
@@ -0,0 +1,29 @@
+/**
+ * @module utils/alerts
+ */
+
+import { color, label, log, symbol } from "./index.js";
+
+/**
+ * Display an alert message in the console.
+ *
+ * @param {string} type - The type of alert to display [error, info, note, success, warning].
+ * @param {string} msg - The message to display.
+ * @param {string} title - The title of the alert message (defaults to type name).
+ */
+export function alert(type, msg, title = "") {
+  const types = ["error", "info", "note", "success", "warning"];
+
+  if (!types.includes(type)) {
+    throw new Error("Invalid alert type.");
+  }
+
+  const icon = symbol[type];
+
+  title = title ? title.toUpperCase() : type.toUpperCase();
+  title = label[type](` ${title} `);
+
+  msg = color[type](msg);
+
+  log(`\n${icon} ${title} ${msg}\n`);
+}
